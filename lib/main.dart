4baffca86c5d241680f5534696b5ff6fb94f91b0
DESKTOP-0HQK47B\Mahide <mahidekarim@gmail.com>
2021-10-11 12:00:54 +0600

    first two page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:travel/singup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,44 +10,180 @@ void main() async {
   runApp(MyApp());
 }
 
-// For testing the link with firebase.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'History Client',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('History Client App'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => FirebaseFirestore.instance
-              .collection('testing')
-              .add({'timestamp': Timestamp.fromDate(DateTime.now())}),
-          child: Icon(Icons.add),
-        ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('testing').snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return const SizedBox.shrink();
-            }
-            return ListView.builder(
-              itemCount: snapshot.data?.docs.length ?? 0,
-              itemBuilder: (BuildContext context, int index) {
-                final docData = snapshot.data?.docs[index].data();
-                // final dateTime = (docData!['timestamp']! as Timestamp).toDate();
-                return ListTile(
-                  title: Text(docData.toString()),
-                );
-              },
-            );
-          },
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+      title: 'LoginPage',
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text("LoginPage"),
+      // ),
+      body: Container(
+        color: Colors.white,
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 64, left: 16),
+              child: Text(
+                "Welcome",
+                style: TextStyle(fontSize: 25, color: Color(0xff3a5e44)),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 4, left: 16),
+              child: Text(
+                "Log in to continue",
+                style: TextStyle(fontSize: 10, color: Color(0xff3a5e44)),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 20),
+              child: Image(
+                image: AssetImage("assets/car.png"),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.only(top: 16,right: 16,left: 24),
+              child: Text(
+                "Login",style: TextStyle(color: Color(0xff3a5e44),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              height: 60,
+              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.attach_email,
+                      color: Colors.grey,
+                    ),
+                    border: InputBorder.none,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      borderSide: BorderSide(color: Color(0xff3a5e44)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      borderSide: BorderSide(color: Color(0xff3a5e44)),
+                    ),
+
+                    // border: OutlineInputBorder(),
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Color(0xff3a5e44))),
+              ),
+            ),
+            Container(
+              height: 60,
+              padding: EdgeInsets.only(top: 10, left: 16, right: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.grey,
+                    ),
+                    border: InputBorder.none,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      borderSide: BorderSide(color: Color(0xff3a5e44)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      borderSide: BorderSide(color: Color(0xff3a5e44)),
+                    ),
+                    // border: OutlineInputBorder(),
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Color(0xff3a5e44))),
+              ),
+            ),
+            Container(
+              alignment: Alignment.topRight,
+              margin: EdgeInsets.only(top: 4),
+              child: FlatButton(
+                onPressed: () {
+                  // forgotPass(context);
+                },
+                textColor: Colors.grey,
+                child: Text(
+                  "Forgot Password?",
+                  style: TextStyle(),
+                ),
+              ),
+            ),
+            Container(
+              height: 56,
+              alignment: Alignment.center,
+              // margin: EdgeInsets.only(left: 48,right: 48),
+              child: MaterialButton(
+                  color: Color(0xff3a5e44),
+                  onPressed: () {  },
+                  child: Image(
+                    image: AssetImage("assets/login.png",),height: 80,width: 180,
+                  )
+              ),
+            ),
+            Container(
+              child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 24),
+                  child: Image(
+                    image: AssetImage("assets/logo.png"),height: 50,width: 150,
+                  )),
+            ),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 16),
+              child: FlatButton(
+                onPressed: () {},
+                textColor: Colors.grey,
+                child: Text(
+                  "Don't have account?",
+                  style: TextStyle(),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              //margin: EdgeInsets.only(top: 0),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SignUp()),
+                  );
+
+                },
+                textColor: Color(0xff3a5e44),
+                child: Text(
+                  "Sign up",
+                  style: TextStyle(),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
