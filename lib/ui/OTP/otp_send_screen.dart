10d8622ba1113588828fc1db718c491ef4458f9c
DESKTOP-4PFVMS4\Shimanto Ahmed <afser@parallaxlogic.com>
2021-10-18 21:38:38 +0600

    bugfix

--- lib/ui/OTP/otp_send_screen.dart
+++ lib/ui/OTP/otp_send_screen.dart
@@ -28,7 +28,7 @@ class _OtpSendPageState extends State<OtpSendPage> {
   TextEditingController _inputNumberTwoController = TextEditingController();
   TextEditingController _inputNumberThreeController = TextEditingController();
   TextEditingController _inputNumberFourController = TextEditingController();
-
+  var hasError=0;
   var otp = "0011";
 
   late FocusNode pin2FocusNode;
@@ -110,6 +110,12 @@ class _OtpSendPageState extends State<OtpSendPage> {
                         flex: 5,
                         child: Container(
                           child: TextFormField(
+                            validator: (value){
+                              if(value!.isEmpty){
+                                hasError++;
+                              }
+                              return null;
+                            },
                             controller: _inputNumberOneController,
                             // focusNode: pin2FocusNode,
                             autofocus: true,
@@ -149,6 +155,12 @@ class _OtpSendPageState extends State<OtpSendPage> {
                         flex: 5,
                         child: Container(
                           child: TextFormField(
+                            validator: (value){
+                              if(value!.isEmpty){
+                                hasError++;
+                              }
+                              return null;
+                            },
                             controller: _inputNumberTwoController,
                             focusNode: pin2FocusNode,
                             autofocus: true,
@@ -185,6 +197,12 @@ class _OtpSendPageState extends State<OtpSendPage> {
                         flex: 5,
                         child: Container(
                           child: TextFormField(
+                            validator: (value){
+                              if(value!.isEmpty){
+                                hasError++;
+                              }
+                              return null;
+                            },
                             controller: _inputNumberThreeController,
                             focusNode: pin3FocusNode,
                             autofocus: true,
@@ -221,6 +239,12 @@ class _OtpSendPageState extends State<OtpSendPage> {
                         flex: 5,
                         child: Container(
                           child: TextFormField(
+                            validator: (value){
+                              if(value!.isEmpty){
+                                hasError++;
+                              }
+                              return null;
+                            },
                             controller: _inputNumberFourController,
                             focusNode: pin4FocusNode,
                             autofocus: true,
@@ -300,7 +324,8 @@ class _OtpSendPageState extends State<OtpSendPage> {
   Widget bottomButton() {
     return MaterialButton(
       onPressed: () {
-        if (_formKey.currentState!.validate()) {
+        print("clicked and hasError "+hasError.toString());
+        if (_formKey.currentState!.validate() && hasError==0) {
           var inputValues = _inputNumberOneController.text +
               _inputNumberTwoController.text +
               _inputNumberThreeController.text +
