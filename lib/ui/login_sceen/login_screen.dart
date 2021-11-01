53d80542ab95875d4bc972331ac39d460723b962
DESKTOP-0HQK47B\Mahide <mahidekarim@gmail.com>
2021-11-01 17:12:27 +0600

    login done

--- lib/ui/login_sceen/login_screen.dart
+++ lib/ui/login_sceen/login_screen.dart
@@ -27,105 +27,131 @@ class _LoginScreenState extends State<LoginScreen> {
       // ),
       body: SingleChildScrollView(
         child: Container(
-          margin: EdgeInsets.only(bottom: 24),
+          //  margin: EdgeInsets.only(bottom: 24),
           color: Colors.white,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                padding: EdgeInsets.only(top: 64, left: 16),
+                padding: EdgeInsets.only(top: 64, left: 24),
                 child: Text(
-                  "Welcome",
-                  style: TextStyle(fontSize: 25, color: Color(0xff3a5e44)),
+                  "Welcome Back",
+                  style: TextStyle(
+                    fontFamily: 'Raleway',
+                    color: Color(0xff3a5e44),
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    fontStyle: FontStyle.italic,
+                  ),
                 ),
               ),
               Container(
-                padding: EdgeInsets.only(top: 4, left: 16),
+                padding: EdgeInsets.only(top: 0.7, left: 25.2),
                 child: Text(
                   "Log in to continue",
-                  style: TextStyle(fontSize: 10, color: Color(0xff3a5e44)),
+                  style: TextStyle(
+                      fontFamily: 'Raleway',
+                      color: Color(0xff3a5e44),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal),
                 ),
               ),
               Container(
                 alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 20),
+                margin: EdgeInsets.only(top: 17.7, left: 41, right: 41),
                 child: Image(
                   image: AssetImage("assets/car.png"),
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(top: 16, right: 16, left: 24),
+                margin: EdgeInsets.only(top: 25.4, left: 18.3),
                 child: Text(
                   "Login",
                   style: TextStyle(
-                      color: Color(0xff3a5e44),
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
+                    fontFamily: 'Roboto',
+                    color: Color(0xff3a5e44),
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                    fontStyle: FontStyle.normal,
+                  ),
                 ),
               ),
               Form(
                 key: _formKey,
                 child: Container(
-                  height: 62,
-                  padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                  height: 65,
+                  padding: EdgeInsets.only(top: 12.1, left: 18.3, right: 18.3),
                   child: TextFormField(
                     validator: (value) {
                       return null;
                     },
                     controller: _emailController,
                     decoration: InputDecoration(
-                        // prefixIcon: Icon(
-                        //   Icons.attach_email,
-                        //   color: Colors.grey,
-                        // ),
-                        border: InputBorder.none,
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          borderSide: BorderSide(color: Color(0xff3a5e44)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          borderSide: BorderSide(color: Color(0xff3a5e44)),
-                        ),
+                      // prefixIcon: Icon(
+                      //   Icons.attach_email,
+                      //   color: Colors.grey,
+                      // ),
+                      border: InputBorder.none,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(color: Color(0xff3a5e44)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(color: Color(0xff3a5e44)),
+                      ),
 
-                        // border: OutlineInputBorder(),
-                        labelText: "Email",
-                        labelStyle:
-                            TextStyle(color: Color(0xff3a5e44), fontSize: 12)),
+                      // border: OutlineInputBorder(),
+                      labelText: "Email",
+                      labelStyle: TextStyle(
+                        fontFamily: 'Roboto',
+                        color: Color(0xff3a5e44),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
                   ),
                 ),
               ),
               Container(
-                height: 60,
-                padding: EdgeInsets.only(top: 10, left: 16, right: 16),
+                height: 65,
+                padding: EdgeInsets.only(top: 12.1, left: 18.3, right: 18.3),
                 child: TextFormField(
                   controller: _passController,
                   validator: (value) {
                     return null;
                   },
                   decoration: InputDecoration(
-                      // prefixIcon: Icon(
-                      //   Icons.lock,
-                      //   color: Colors.grey,
-                      // ),
-                      border: InputBorder.none,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                        borderSide: BorderSide(color: Color(0xff3a5e44)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                        borderSide: BorderSide(color: Color(0xff3a5e44)),
-                      ),
-                      // border: OutlineInputBorder(),
-                      labelText: "Password",
-                      labelStyle:
-                          TextStyle(color: Color(0xff3a5e44), fontSize: 12)),
+                    // prefixIcon: Icon(
+                    //   Icons.lock,
+                    //   color: Colors.grey,
+                    // ),
+                    border: InputBorder.none,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Color(0xff3a5e44)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Color(0xff3a5e44)),
+                    ),
+                    // border: OutlineInputBorder(),
+                    labelText: "Password",
+                    labelStyle: TextStyle(
+                      fontFamily: 'Roboto',
+                      color: Color(0xff3a5e44),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
                 ),
               ),
               Container(
                 alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 50),
+                margin: EdgeInsets.only(top: 35.9),
                 child: MaterialButton(
                   onPressed: () {
                     Navigator.push(
@@ -137,16 +163,19 @@ class _LoginScreenState extends State<LoginScreen> {
                   child: Text(
                     "Forgot Password",
                     style: TextStyle(
+                        fontFamily: 'Roboto',
                         color: Color(0xff3a5e44),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.normal,
                         decoration: TextDecoration.underline,
                         decorationThickness: 2),
                   ),
                 ),
               ),
               Container(
-                // height: 56,
                 alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 10),
+                margin: EdgeInsets.only(top: 15.6),
                 child: MaterialButton(
                   color: Color(0xff3a5e44),
                   onPressed: () {
@@ -169,26 +198,41 @@ class _LoginScreenState extends State<LoginScreen> {
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10)),
                   child: Container(
-                      height: 35,
-                      width: 150,
-                      margin: EdgeInsets.only(top: 15),
-                      child: Text("Login",
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                          textAlign: TextAlign.center)),
+                    alignment: Alignment.center,
+                    height: 40,
+                    width: 160,
+                    margin: EdgeInsets.only(top: 8, bottom: 8),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontFamily: 'Raleway',
+                        color: Color(0xfffcfcfc),
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                  ),
                 ),
               ),
               Container(
                 alignment: Alignment.center,
-                margin: (EdgeInsets.only(top: 25)),
+                margin: (EdgeInsets.only(top: 12.4)),
                 child: Text(
                   "Or Login with",
-                  style: TextStyle(color: Color(0xff3a5e44), fontSize: 12),
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    color: Color(0xff3a5e44),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                  ),
                 ),
               ),
               Container(
                 child: Container(
                     alignment: Alignment.center,
-                    margin: EdgeInsets.only(top: 24),
+                    margin: EdgeInsets.only(top: 19),
                     child: Image(
                       image: AssetImage("assets/logo.png"),
                       height: 50,
@@ -196,22 +240,30 @@ class _LoginScreenState extends State<LoginScreen> {
                     )),
               ),
               Container(
-                margin: EdgeInsets.only(top: 16),
+                margin: EdgeInsets.only(top: 14.9),
                 child: Center(
                   child: RichText(
                     text: TextSpan(
                         text: "Don't have an Account? ",
                         style: TextStyle(
-                          color: Colors.grey,
+                          fontFamily: 'Roboto',
+                          color: Color(0xff3a5e44),
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
                         ),
                         children: [
                           TextSpan(
                               text: "Signup",
                               style: TextStyle(
-                                  color: Color(0xff3a5e44),
-                                  decoration: TextDecoration.underline,
-                                  decorationThickness: 2,
-                                  fontWeight: FontWeight.bold),
+                                fontFamily: 'Roboto',
+                                color: Color(0xff3a5e44),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                fontStyle: FontStyle.normal,
+                                decoration: TextDecoration.underline,
+                                decorationThickness: 2,
+                              ),
                               recognizer: TapGestureRecognizer()
                                 ..onTap = () {
                                   Navigator.push(
@@ -224,37 +276,6 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     Container(
-              //      // margin: EdgeInsets.only(top: 16),
-              //       child: MaterialButton(
-              //         onPressed: () {},
-              //         textColor: Colors.grey,
-              //         child: Text(
-              //           "Don't have account?",
-              //           style: TextStyle(),
-              //         ),
-              //       ),
-              //     ),
-              //     Container(
-              //       child: MaterialButton(
-              //         onPressed: () {
-              //           Navigator.push(
-              //             context,
-              //             MaterialPageRoute(builder: (context) => SignUp()),
-              //           );
-              //         },
-              //         textColor: MyColors.primaryColor,
-              //         child: Text(
-              //           "Sign up",
-              //           style: TextStyle(),
-              //         ),
-              //       ),
-              //     ),
-              //   ],
-              // ),
             ],
           ),
         ),
